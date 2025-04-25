c206a2036325983b65489589716a724e216c4aad
saqib147 <saqibarshad429@gmail.com>
2025-04-25 15:10:31 +0500

    Remove autoprefixer from Vite configuration plugins

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 import autoprefixer from "autoprefixer";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), autoprefixer()],
+  plugins: [react(), tailwindcss()],
 });
